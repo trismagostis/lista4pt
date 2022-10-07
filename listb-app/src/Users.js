ab8cc5cb5c05387cfa79fc32b69e53fae84d015d
trismagostis <piotr.kaczmarzyk@poczta.fm>
2022-10-07 11:53:09 +0200

    init

---
+++ listb-app/src/Users.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./Users.css";
+import UsersList from "./UsersList";
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: [] };
+    this.myRef = React.createRef();
+  }
+  addUser = (event) => {
+    event.preventDefault();
+    let newUser = this.myRef.current.value;
+    this.myRef.current.value = "";
+    this.setState((state) => {
+      return { users: state.users.concat(newUser) };
+    });
+  };
+  deleteUser = (userIndex) => {
+    // console.log(userIndex);
+    this.setState((state) => {
+      return {
+        users: state.users.filter((user,index) => index !== userIndex)
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="main">
+        <h1>Users List</h1>
+        <form onSubmit={this.addUser}>
+          <input ref={this.myRef} type="text" placeholder="Enter name" />
+          <button type="submit">Add user</button>
+        </form>
+        <UsersList
+          usersList={this.state.users}
+          deleteUserMethod={this.deleteUser}
+        />
+      </div>
+    );
+  }
+}
+export default Users;
