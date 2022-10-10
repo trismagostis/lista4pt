325787fb102c0fb9d6e33aae3ad3cd016a41b1e2
trismagostis <piotr.kaczmarzyk@poczta.fm>
2022-10-10 13:40:58 +0200

    initial

--- listb-app/src/App.js
+++ listb-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import UsersStates from './UsersStates';
 import Users from './Users';
 
 function App() {
   return (
     <div className="App">
-      <Users/>
+      <UsersStates/>
     </div>
   );
 }
---
+++ listb-app/src/UsersStates.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./Users.css";
+import UsersList from "./UsersList";
+
+const UsersStates = () => {
+  const [users, setUsers] = useState([]);
+  let myRef = React.createRef();
+  const deleteUser = (userIndex) => {
+    // console.log(userIndex);
+    setUsers(users.filter((user, index) => index !== userIndex));
+  };
+
+  const addUser = (event) => {
+    event.preventDefault();
+    let newUser = myRef.current.value;
+    myRef.current.value = "";
+    if (newUser !== "") {
+      setUsers(users.concat(newUser));
+    } else {
+      alert("puste");
+    }
+  };
+
+  return (
+    <div className="main">
+      <h1>Users List</h1>
+      <form onSubmit={addUser}>
+        <input
+          ref={myRef}
+          type="text"
+          placeholder="Enter name"
+          maxlength="30"
+        />
+        <button type="submit">Add user</button>
+      </form>
+      <UsersList usersList={users} deleteUserMethod={deleteUser} />
+    </div>
+  );
+};
+
+export default UsersStates;
